8f64e46ad9eed3c24c1899d4d80aa60a0f563e93
Norbert Forero <nsforero@sanmateo.edu.co>
2019-11-12 14:27:46 -0500

    Merge con login funcional y validadores

--- RegistroLogin/login.php
+++ RegistroLogin/login.php
@@ -10,17 +10,14 @@
 
 
 <!DOCTYPE HTML>
-<!--
-	Identity by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
+
 <html>
 	<head>
 		<title>Swapp</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/css/main.css" />
+		<script src = " https://unpkg.com/sweetalert/dist/sweetalert.min.js " > </script>
 		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 	</head>
 	<body class="is-preload">
